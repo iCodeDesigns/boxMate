6b5dbb18e6275fcaf63a9983ae097d17efc563ed
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2021-02-02 14:37:51 +0200

    created_models_for_taxes

--- boxMate/codes/models.py
+++ boxMate/codes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class TaxTypes(models.Model):
+    code = models.CharField(max_length=4 , unique=True)
+    desc_en = models.CharField(max_length=50)
+    desc_ar = models.CharField(max_length=50)
+    is_taxable = models.BooleanField()
+
+class TaxSubtypes(models.Model):
+    code = models.CharField(max_length=4 , unique=True)
+    desc_en = models.CharField(max_length=50)
+    desc_ar = models.CharField(max_length=50)
+    taxtype_reference = models.ForeignKey(TaxTypes , on_delete=models.CASCADE)
