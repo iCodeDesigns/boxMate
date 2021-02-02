0c67ec60b696ab4339ce8968eb829309488a7209
aahd Hozayen <ahd.hozayen@mashreqarabia.com>
2021-02-02 17:51:36 +0200

    fix import resources

--- boxMate/codes/resources.py
+++ boxMate/codes/resources.py
@@ -7,8 +7,9 @@ class ActivityTypeResource(resources.ModelResource):
     # this class describes how the model will be imported
     class Meta:
         model = ActivityType
-        fields = ('code', 'desc_en', 'desc_ar')
+        # fields = ('code', 'desc_en', 'desc_ar')
         exclude = ('id',)
+        import_id_fields = ('code', 'desc_en', 'desc_ar')
           # defines which model fields will be imported
         # id is required here to save attendance object
 
@@ -25,63 +26,63 @@ class ActivityTypeResource(resources.ModelResource):
 class CountryCodeResource(resources.ModelResource):
     class Meta:
         model = CountryCode
-        fields = ('code', 'desc_en', 'desc_ar') 
+        fields = ('code', 'desc_en', 'desc_ar')
 
     desc_en = fields.Field(
-        column_name='Desc_en',  
+        column_name='Desc_en',
         attribute='desc_en',)
-    
+
     desc_ar = fields.Field(
-        column_name='Desc_ar',  
-        attribute='desc_ar',)    
+        column_name='Desc_ar',
+        attribute='desc_ar',)
 
 
 
 class UnitTypeResource(resources.ModelResource):
     class Meta:
         model = UnitType
-        fields = ('code', 'desc_en', 'desc_ar') 
+        fields = ('code', 'desc_en', 'desc_ar')
 
     desc_en = fields.Field(
-        column_name='desc_en', 
+        column_name='desc_en',
         attribute='desc_en',)
-    
+
     desc_ar = fields.Field(
-        column_name='desc_ar',  
-        attribute='desc_ar',)    
+        column_name='desc_ar',
+        attribute='desc_ar',)
 
 
 
 class TaxTypeResource(resources.ModelResource):
     class Meta:
         model = TaxTypes
-        fields = ('code', 'desc_en', 'desc_ar')  
+        fields = ('code', 'desc_en', 'desc_ar')
 
     desc_en = fields.Field(
-        column_name='Desc_en',  
+        column_name='Desc_en',
         attribute='desc_en',)
-    
+
     desc_ar = fields.Field(
-        column_name='Desc_ar', 
+        column_name='Desc_ar',
         attribute='desc_ar',)
 
-            
+
 
 class TaxSubtypeResource(resources.ModelResource):
     class Meta:
         model = TaxSubtypes
-        fields = ('code', 'desc_en', 'desc_ar', 'taxtype_reference')  
+        fields = ('code', 'desc_en', 'desc_ar', 'taxtype_reference')
 
     desc_en = fields.Field(
-        column_name='Desc_en',  
+        column_name='Desc_en',
         attribute='desc_en',)
-    
+
     desc_ar = fields.Field(
-        column_name='Desc_ar',  
-        attribute='desc_ar',)        
+        column_name='Desc_ar',
+        attribute='desc_ar',)
 
 
     taxtype_reference = fields.Field(
-        column_name='TaxtypeReference',  
-        attribute='taxtype_reference',  
-        widget=ForeignKeyWidget(TaxTypes, 'pk')) 
+        column_name='TaxtypeReference',
+        attribute='taxtype_reference',
+        widget=ForeignKeyWidget(TaxTypes, 'pk'))
