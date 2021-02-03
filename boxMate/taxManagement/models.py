085bffc59f9f9b7f48a81b4445578f80ac84ee67
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2021-02-03 11:44:59 +0200

    added_reciever_and_invoice_header_models

--- boxMate/taxManagement/models.py
+++ boxMate/taxManagement/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from issuer.models import Issuer,Reciever
+from codes.models import ActivityType 
 
-# Create your models here.
+class InvoiceHeader(models.Model):
+    issuer = models.ForeignKey(Issuer , on_delete=models.CASCADE)
+    reciever = models.ForeignKey(Reciever , on_delete=models.CASCADE)
+    document_type = models.CharField(max_length=2 ,
+                                    choices=[('i','invoice')] , default='i')
+    document_type_version = models.CharField(max_length=8, 
+                                        choices=[('1.0','1.0')],default='1.0')
+    
+    date_time_issued = models.DateTimeField(auto_now_add=True)
+    taxpayer_activity_code = models.ForeignKey(ActivityType , on_delete=models.CASCADE)
+    internal_id = models.CharField(max_length=50)
+    purchase_order_reference = models.CharField(max_length=50)
+    purchase_order_description = models.CharField(max_length=100)
+    sales_order_reference = models.CharField(max_length=50)
+    sales_order_description = models.CharField(max_length=100)
+    proforma_invoice_number = models.CharField(max_length=50)
+    total_sales_amount = models.DecimalField(decimal_places=5,max_digits=20)
+    total_discount_amount = models.DecimalField(decimal_places=5,max_digits=20)
+    net_amount = models.DecimalField(decimal_places=5,max_digits=20)
+    extra_discount_amount = models.DecimalField(decimal_places=5,max_digits=20)
+    total_items_discount_amount	= models.DecimalField(decimal_places=5,max_digits=20)
+    total_amount = models.DecimalField(decimal_places=5,max_digits=20)
+    signature = models.TextField()
+        
