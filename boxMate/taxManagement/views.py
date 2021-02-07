968700eb8da8f2a505b54eb202e6c43ca1fde41a
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2021-02-07 12:58:54 +0200

    added data from main table to tax lines table

--- boxMate/taxManagement/views.py
+++ boxMate/taxManagement/views.py
@@ -8,9 +8,9 @@ from tablib import Dataset
 from django.conf import settings
 from taxManagement.tmp_storage import TempFolderStorage
 from django.db.models import Count
-from .models import MainTable, InvoiceHeader ,InvoiceLine
+from .models import MainTable, InvoiceHeader ,InvoiceLine , TaxTypes,TaxLine
 from issuer.models import Issuer,Receiver
-from codes.models import ActivityType
+from codes.models import ActivityType , TaxSubtypes, TaxTypes
 from rest_framework.decorators import api_view
 
 
@@ -30,7 +30,7 @@ def write_to_tmp_storage(import_file):
 
 
 
-@api_view(['POST', ])
+# @api_view(['POST', ])
 def import_data_to_invoice():
     #### to be tested ####
     headers = MainTable.objects.values('document_type','document_type_version',
@@ -42,7 +42,7 @@ def import_data_to_invoice():
     'signature_type').annotate(Count('internal_id'))
     for header in headers:
         issuer = Issuer.objects.get(reg_num=header['issuer_registration_num'])
-        reciever = Receiver.objects.get(reg_num=header['receiver_registration_num'])
+        receiver = Receiver.objects.get(reg_num=header['receiver_registration_num'])
         taxpayer_activity_code = ActivityType.objects.get(code=header['taxpayer_activity_code'])
         header_obj = InvoiceHeader(
             issuer = issuer,
@@ -66,30 +66,49 @@ def import_data_to_invoice():
             signature= header['signature_type']
         )
         header_obj.save()
-        lines = MainTable.objects.filter(invoice_header=header_obj)
+        ####### create lines per invoice header #######
+        lines = MainTable.objects.values('description','item_code','item_type',
+        'unit_type','quantity', 'sales_total','currency_sold','amount_egp',
+        'amount_sold','currency_exchange_rate','total','value_difference',
+        'total_taxable_fees','items_discount','net_total','discount_rate',
+        'discount_amount','internal_code').annotate(Count('item_code'))
         for line in lines:
             line_obj = InvoiceLine(
                 invoice_header=header_obj,
-                description = line.description,
-                itemType = line.item_type,
-                itemCode = line.item_code,
-                unitType = line.unit_type,
-                quantity = line.quantity,
-                currencySold = line.currency_sold,
-                amountEGP = line.amount_egp,
-                amountSold = line.amount_sold,
-                currencyExchangeRate = line.currency_exchange_rate,
-                salesTotal = line.sales_total,
-                total = line.total,
-                valueDifference = line.value_difference,
-                totalTaxableFees = line.total_taxable_fees,
-                itemsDiscount = line.items_discount,
-                netTotal = line.net_total,
-                rate = line.rate,
-                amount = line.amount,
-                internalCode = line.internal_code,
+                description = line['description'],
+                itemType = line['item_type'],
+                itemCode = line['item_code'],
+                unitType = line['unit_type'],
+                quantity = line['quantity'],
+                currencySold = line['currency_sold'],
+                amountEGP = line['amount_egp'],
+                amountSold = line['amount_sold'],
+                currencyExchangeRate = line['currency_exchange_rate'],
+                salesTotal = line['sales_total'],
+                total = line['total'],
+                valueDifference = line['value_difference'],
+                totalTaxableFees = line['total_taxable_fees'],
+                itemsDiscount = line['items_discount'],
+                netTotal = line['net_total'],
+                rate = line['discount_rate'],
+                amount = line['discount_amount'],
+                internalCode = line['internal_code'],
             )
             line_obj.save()
+            ##### create tax lines per invoice line #####
+            tax_types = MainTable.objects.values('taxt_item_type','tax_item_amount',
+            'tax_item_subtype','tax_item_rate').annotate(Count('internal_id')).annotate(Count('item_code'))
+            for tax_type in tax_types:
+                tax_main_type = TaxTypes.objects.get(code = tax_type['taxt_item_type'])
+                tax_subtype = TaxSubtypes.objects.get(code = tax_type['tax_item_subtype'])
+                tax_type_obj = TaxLine(
+                    invoice_line=line_obj,
+                    taxType = tax_main_type,
+                    subType = tax_subtype,
+                    amount = tax_type['tax_item_amount'],
+                    rate = tax_type['tax_item_rate']
+                )
+                tax_type_obj.save()
 # Create your views here.
 @api_view(['POST', ])
 def upload_excel_sheet(request):
