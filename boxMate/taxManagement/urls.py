bf346f8f4e622c30196d1ea021af06e4140c2516
aahd Hozayen <ahd.hozayen@mashreqarabia.com>
2021-02-09 20:08:13 +0200

    final status for aahd

--- boxMate/taxManagement/urls.py
+++ boxMate/taxManagement/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from taxManagement import views
 
-app_name = 'tax'
+app_name = 'taxManagement'
 urlpatterns = [
     path('upload/', views.upload_excel_sheet, name='upload-tax'),
-    path('invoice-headers/' , views.get_all_invoice_headers , name='get-all-invoice-headers')
+    # path('invoice-headers/' , views.get_all_invoice_headers , name='get-all-invoice-headers')
 
 ]
