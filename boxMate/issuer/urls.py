2f01c7d39e4d9a1628572818e16a21b3ed39473d
gehad <jihadfathalla3@gmail.com>
2021-02-10 12:53:39 +0200

    save_submition

--- boxMate/issuer/urls.py
+++ boxMate/issuer/urls.py
@@ -5,8 +5,6 @@ from issuer import views
 app_name = 'issuer'
 
 urlpatterns = [
-    path('create/', views.add_issuer, name='create-issuer'),
-    path('list/', views.IssuerListView.as_view(), name='list-issuers'),
     path('list/uploaded-invoices', views.list_uploaded_invoice, name='list-uploaded-invoices'),
 ]
     
--- boxMate/taxManagement/urls.py
+++ boxMate/taxManagement/urls.py
@@ -6,8 +6,9 @@ app_name = 'taxManagement'
 urlpatterns = [
     path('upload/', views.upload_excel_sheet, name='upload-tax'),
     path('subm_list/', views.submission_list, name='submission-list'),
-    path('invoice-headers/' , views.get_all_invoice_headers , name='get-all-invoice-headers')
+    path('invoice-headers/' , views.get_all_invoice_headers , name='get-all-invoice-headers'),
     # path('invoice-headers/' , views.get_all_invoice_headers , name='get-all-invoice-headers')
     path('list/eta-invoice/', views.list_eta_invoice, name='list-eta-invoice'),
+    path('test/', views.submit_invoice),
 
-]
+] 
