2ca7b5c459c622653f3759c74b2783661f481d6a
amira <amira.a.a.hashim@gmail.com>
2021-04-13 12:05:18 +0200

    separate portal requests in a new file

--- boxMate/taxManagement/views.py
+++ boxMate/taxManagement/views.py
@@ -34,6 +34,7 @@ from issuer.decorators import is_issuer
 from currencies.models import Currency
 import zipfile
 from django.utils.translation import ugettext_lazy as _
+from .portal_api import *
 
 TMP_STORAGE_CLASS = getattr(settings, 'IMPORT_EXPORT_TMP_STORAGE_CLASS',
                             TempFolderStorage)
@@ -243,50 +244,6 @@ def upload_excel_sheet(request):
         return redirect('/tax/list/uploaded-invoices')
 
 
-def get_submission_response(submission_id):
-    '''
-    This function is used to check and save the overall status of the submission ,
-    doc number,doc_uuid,doc_count and time received.
-
-    :param submission_id: the subm_id of the invoice to save its submission data
-    :return: response object from the gov api
-    '''
-    url = 'https://api.preprod.invoicing.eta.gov.eg/api/v1.0/documentSubmissions/' + \
-          submission_id + '?PageSize=1'
-    response = requests.get(url, verify=False,
-                            headers={'Authorization': 'Bearer ' + auth_token, }
-                            )
-    # if token is expired
-    if response.status_code == status.HTTP_401_UNAUTHORIZED:
-        get_token()
-        response = requests.get(url, verify=False,
-                                headers={
-                                    'Authorization': 'Bearer ' + auth_token, }
-                                )
-
-    # in case of network error
-    # TODO handle connection issues with the gov api
-    if (response.status_code != status.HTTP_200_OK):
-        time.sleep(20)  # wait some time and try again
-        response = requests.get(url, verify=False,
-                                headers={
-                                    'Authorization': 'Bearer ' + auth_token, }
-                                )
-    response_code = response
-    response_json = response_code.json()
-
-    submission = Submission.objects.get(subm_id=submission_id)
-
-    # document uuid
-    submission.subm_uuid = response_json['documentSummary'][0]['uuid']
-    submission.document_count = response_json['documentCount']
-    submission.date_time_received = response_json['dateTimeReceived']
-    submission.over_all_status = response_json['overallStatus']
-    submission.save()
-
-    return response
-
-
 def save_submission_response(invoice_id, submission_id, status):
     '''
     :param invoice_id: the id of the invoice to save its submission (db id)
@@ -334,28 +291,6 @@ def update_invoice_doc_version(invoice_id, version):
         invoice_header.save()
 
 
-def send_data_by_version(data):
-    """
-    send data to portal according to document version id
-    :param data: data required
-    :return: response
-    by: amira
-    date: 8/4/2021
-    """
-    url = 'https://api.preprod.invoicing.eta.gov.eg/api/v1/documentsubmissions'
-    response = requests.post(url, verify=False,
-                             headers={'Content-Type': 'application/json',
-                                      'Authorization': 'Bearer ' + auth_token},
-                             data=data)
-    if response.status_code == status.HTTP_401_UNAUTHORIZED:
-        get_token()
-        response = requests.post(url, verify=False,
-                                 headers={'Content-Type': 'application/json',
-                                          'Authorization': 'Bearer ' + auth_token},
-                                 data=data)
-    return response
-
-
 @login_required(login_url='home:user-login')
 @is_issuer
 def submit_invoice(request, invoice_id, version=None):
@@ -373,14 +308,16 @@ def submit_invoice(request, invoice_id, version=None):
         version = InvoiceHeader.objects.get(id=invoice_id).document_type_version
     # function that gets the invoice data in JSON format
     generated_invoice = Invoicegeneration(invoice_id=invoice_id).get_one_invoice()
+    print('wooooow')
     invoice_as_str = simplejson.dumps(generated_invoice)  # by:ahd hozayen, we used simplejson to decode Decimal fields
-
+    print('invoice ', invoice_as_str)
     if version == '1.0':
         jar = call_java.java_func(invoice_as_str, "Dreem", "08268939")  # send invoice_as_str to jar file to sign it.
         from_bytes_to_good_str = jar.decode("utf-8")
         from_str_to_json = json.loads(json.dumps(from_bytes_to_good_str))
         response = send_data_by_version(data=from_str_to_json)
     elif version == '0.9':
+        print('version: ', version)
         response = send_data_by_version(data=invoice_as_str)
     else:
         response = ''
@@ -428,18 +365,7 @@ def get_all_invoice_headers(request):
 def get_decument_detail_after_submit(request, internal_id):
     submission = Submission.objects.get(invoice__id=internal_id)
     if submission.subm_uuid is not None:
-        url = 'https://api.preprod.invoicing.eta.gov.eg/api/v1/documents/' + \
-              submission.subm_uuid + '/details'
-        response = requests.get(url, verify=False,
-                                headers={
-                                    'Authorization': 'Bearer ' + auth_token, }
-                                )
-        if response.status_code == status.HTTP_401_UNAUTHORIZED:
-            get_token()
-            response = requests.get(url, verify=False,
-                                    headers={
-                                        'Authorization': 'Bearer ' + auth_token, }
-                                    )
+        response = get_document_details(submission_uuid=submission.subm_uuid)
 
         validation_steps = response.json(
         )['validationResults']['validationSteps']
@@ -475,20 +401,6 @@ def list_eta_invoice(request):
         "eta_invoice_list": eta_invoice_list,
     }
     return render(request, 'eta-invoice.html', eta_context)
-
-
-def get_token():
-    url = "https://id.preprod.eta.gov.eg/connect/token"
-    client_id = "547413a4-79ee-4715-8530-a7ddbe392848"
-    client_secret = "913e5e19-6119-45a1-910f-f060f15e666c"
-    scope = "InvoicingAPI"
-
-    data = {"grant_type": "client_credentials", "client_id": client_id,
-            "client_secret": client_secret, "scope": scope}
-    response = requests.post(url, verify=False,
-                             data=data)
-    global auth_token
-    auth_token = response.json()["access_token"]
 
 
 def resubmit(request, invoice_id):
@@ -531,8 +443,8 @@ def import_data_from_db(request):
         # Make sure an issuer already exists before import
         issuer = Issuer.objects.all()[0]
         print(issuer.activity_code)
-        issuer_address = Address.objects.filter(issuer=issuer)[
-            0]  # Make sure issuer address already exists already exists before import
+        # Make sure issuer address already exists already exists before import
+        issuer_address = Address.objects.filter(issuer=issuer)[0]
         try:
             # Make sure receiver address exist
             receiver = Receiver.objects.get(
@@ -775,24 +687,6 @@ def cancel_document_form(request, doc_uuid):
         'uuid': doc_uuid,
     }
     return render(request, 'cancel_document_form.html', context)
-
-
-def send_cancellation_request(doc_uuid, data):
-    """
-    send cancellation data to portal
-    :param doc_uuid: needed in the url
-    :param data: needed for the request data
-    :return:
-    by: amira
-    date: 12/4/2021
-    """
-    url = f'https://api.preprod.invoicing.eta.gov.eg/api/v1.0/documents/state/{doc_uuid}/state'
-    get_token()
-    response = requests.put(url, verify=False,
-                            headers={'Content-Type': 'application/json',
-                                     'Authorization': 'Bearer ' + auth_token},
-                            data=data)
-    return response
 
 
 def update_submission_and_invoice_status(doc_uuid):
